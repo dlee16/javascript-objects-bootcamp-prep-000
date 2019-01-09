7b94846058e437cbb49ac7e5f61c61d1c1c34ef2
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-09 03:13:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = { artistName: "songTitle"};
 
 function updatePlaylist(playlist, artistName, songTitle){
+  var play= {};
   Object.assign({}, playlist, artistName:"songTitle")
   return playlist
 }
